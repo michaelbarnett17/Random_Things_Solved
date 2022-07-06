6ed3698f5ddfa2e2bde02b4889c1a9f175bd5e39
Michael Barnett <michaelbarnett17@gmail.com>
2022-07-06 11:41:14 -0500

    Added Debugging for Invalid Model State

---
+++ ModelState.IsValid/MyController.cs
@@ -0,0 +1,10 @@
+﻿
+if (!ModelState.IsValid)
+{
+    var message = string.Join(" | ", ModelState.Values
+        .SelectMany(v => v.Errors)
+        .Select(e => e.ErrorMessage));
+    System.Diagnostics.Debug.WriteLine("********************* DEBUG START ******************************");
+    System.Diagnostics.Debug.WriteLine(message);
+    System.Diagnostics.Debug.WriteLine("********************* DEBUG END ********************************");
+}
